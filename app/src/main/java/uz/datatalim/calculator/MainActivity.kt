62fff226c88f2d50e6ea5a455af5d75fbeb6ef41
mavsumbekdev <sgithacker@gmail.com>
2024-10-08 13:27:53 +0500

    xatoliklar tuzatildi

--- app/src/main/java/uz/datatalim/calculator/MainActivity.kt
+++ app/src/main/java/uz/datatalim/calculator/MainActivity.kt
@@ -9,31 +9,30 @@ import android.widget.TextView
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), OnClickListener {
-    lateinit var display: TextView
-    lateinit var delite:Button
-    lateinit var percent:Button
-    lateinit var bNumber0:Button
-    lateinit var bNumber1: Button
-    lateinit var bNumber2: Button
-    lateinit var bNumber3: Button
-    lateinit var bNumber4: Button
-    lateinit var bNumber5: Button
-    lateinit var bNumber6: Button
-    lateinit var bNumber7: Button
-    lateinit var bNumber8: Button
-    lateinit var bNumber9: Button
-    lateinit var bEquals: Button
-    lateinit var bPlus: Button
-    lateinit var bMinus: Button
-    lateinit var divide:Button
-    lateinit var multiplay:Button
-    lateinit var clear:Button
-    lateinit var dot:Button
-    var son1 = 0.0
-    var son2 = 0.0
-    var natija = 0.0
-    var operator:Int=0+1
-    var check=0
+    private lateinit var display: TextView
+    private lateinit var delite:Button
+    private lateinit var percent:Button
+    private lateinit var bNumber0:Button
+    private lateinit var bNumber1: Button
+    private lateinit var bNumber2: Button
+    private lateinit var bNumber3: Button
+    private lateinit var bNumber4: Button
+    private lateinit var bNumber5: Button
+    private lateinit var bNumber6: Button
+    private lateinit var bNumber7: Button
+    private lateinit var bNumber8: Button
+    private lateinit var bNumber9: Button
+    private lateinit var bEquals: Button
+    private lateinit var bPlus: Button
+    private lateinit var bMinus: Button
+    private lateinit var divide:Button
+    private lateinit var multiplay:Button
+    private lateinit var clear:Button
+    private lateinit var dot:Button
+    private var son1 = 0.0
+    private var son2 = 0.0
+    private var natija = 0.0
+    private var operator:Int=0+1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,28 +40,28 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
     }
 
-    fun initViews() {
+    private fun initViews() {
 
-        display = findViewById<TextView>(R.id.tv_display)
-        dot = findViewById<Button>(R.id.dot)
-        bNumber0 = findViewById<Button>(R.id.b_number_0)
-        bNumber1 = findViewById<Button>(R.id.b_number_1)
-        bNumber2 = findViewById<Button>(R.id.b_number_2)
-        bNumber3 = findViewById<Button>(R.id.b_number_3)
-        bNumber4 = findViewById<Button>(R.id.b_number_4)
-        bNumber5 = findViewById<Button>(R.id.b_number_5)
-        bNumber6 = findViewById<Button>(R.id.b_number_6)
-        bNumber7 = findViewById<Button>(R.id.b_number_7)
-        bNumber8 = findViewById<Button>(R.id.b_number_8)
-        bNumber9 = findViewById<Button>(R.id.b_number_9)
+        display = findViewById(R.id.tv_display)
+        dot = findViewById(R.id.dot)
+        bNumber0 = findViewById(R.id.b_number_0)
+        bNumber1 = findViewById(R.id.b_number_1)
+        bNumber2 = findViewById(R.id.b_number_2)
+        bNumber3 = findViewById(R.id.b_number_3)
+        bNumber4 = findViewById(R.id.b_number_4)
+        bNumber5 = findViewById(R.id.b_number_5)
+        bNumber6 = findViewById(R.id.b_number_6)
+        bNumber7 = findViewById(R.id.b_number_7)
+        bNumber8 = findViewById(R.id.b_number_8)
+        bNumber9 = findViewById(R.id.b_number_9)
         bEquals = findViewById(R.id.b_equals)
         bPlus = findViewById(R.id.b_plus)
-        bMinus = findViewById<Button>(R.id.b_minus)
-        divide = findViewById<Button>(R.id.divide)
-        multiplay = findViewById<Button>(R.id.multiply)
-        clear = findViewById<Button>(R.id.clear)
+        bMinus = findViewById(R.id.b_minus)
+        divide = findViewById(R.id.divide)
+        multiplay = findViewById(R.id.multiply)
+        clear = findViewById(R.id.clear)
         percent = findViewById(R.id.percent)
-        delite = findViewById<Button>(R.id.delite)
+        delite = findViewById(R.id.delite)
         dot.setOnClickListener(this)
         delite.setOnClickListener(this)
         bNumber0.setOnClickListener(this)
@@ -87,7 +86,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.b_number_0->{
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "0"
                 }
@@ -96,7 +95,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 }
             }
             R.id.b_number_1 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "1"
                 } else {
@@ -106,7 +105,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_2 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "2"
                 } else {
@@ -115,7 +114,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_3 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "3"
                 } else {
@@ -124,7 +123,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_4 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "4"
                 } else {
@@ -133,7 +132,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_5 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "5"
                 } else {
@@ -142,7 +141,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_6 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "6"
                 } else {
@@ -151,7 +150,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_7 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "7"
                 } else {
@@ -160,7 +159,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_8 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "8"
                 } else {
@@ -169,7 +168,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
 
             R.id.b_number_9 -> {
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "9"
                 } else {
@@ -212,20 +211,18 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 operator=6
                 var backspace:String?=null
                 if (display.text.isNotEmpty()) {
-                    var str:StringBuilder=StringBuilder(display.text)
-                    var find = display.text.toString()
-                    var find2 = find.last()
-//                    if (find2.equals("+")||find2.equals("-")||find2.equals("*")||find2.equals("/")||find2.equals("%")){
-//
-//                    }
-                    str.deleteCharAt(display.text.length-1)
-                    backspace=str.toString()
-                    display.text = backspace
-                    display.text
+                    val str=StringBuilder(display.text)
+                    if (str.length>1&&str.toString()!="0"){
+                        str.deleteCharAt(display.text.length-1)
+                        backspace=str.toString()
+                        display.text = backspace
+                    }else{
+                        display.text="0"
+                    }
                 }
             }
             R.id.dot->{
-                var dText = display.text
+                val dText = display.text
                 if (dText == "0") {
                     display.text = "0"
                 }
